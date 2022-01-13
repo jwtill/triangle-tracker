02eaab00bf1000be969fd6d95f98efe3578950c5
Eric Crudup <briefcasemanx@gmail.com>
2022-01-13 15:39:29 -0800

    added number input form and logic for calculating triangle type
    Co-authored-by: Josh Tillinghast <jwtill@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,40 @@
+function findTriType(length_1, length_2, length_3)
+{
+  
+  if ((length_1 === length_2) && (length_2 === length_3))
+  {
+    $("#results").text("it's equilateral!!!");
+  }
+
+  else if (
+     ((length_1 === length_2) && (length_1 != length_3)) 
+  || ((length_2 === length_3) && (length_2 != length_1)) 
+  || ((length_1 === length_3) && (length_1 != length_2))
+  ) 
+  {
+    $("#results").text("it's isosceles!!!");
+  }
+  else if ((length_1 != length_2) && (length_1 != length_3) && (length_2 != length_3))
+  {
+    $("#results").text("it's scalene!!!");
+  }
+  else if ( 
+        ((length_1 + length_2) <= length_3)
+    ||  ((length_2 + length_3) <= length_1)
+    ||  ((length_1 + length_3) <= length_2)
+  )
+  {
+    $("#results").text("NOT a triangle");
+  }
+}
+
+
 $(document).ready(function() {
-
-
-
+  $("#formOne").submit( function() {
+    event.preventDefault();
+    const length1 = $("#side1").val();
+    const length2 = $("#side2").val();
+    const length3 = $("#side3").val();
+    findTriType(length1, length2, length3);
+  });
 });
